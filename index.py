48cb5c3d696a35b29aec120535e50c967061a5b7
Okomekomekome12 <okomekomekome1@outlook.com>
2026-04-19 18:59:10 +0900

    modified:   index.py

--- index.py
+++ index.py
@@ -5,14 +5,25 @@ import time
 import discord
 import os
 from flask import Flask, request , jsonify
+#discord.py初期設定
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 app = Flask(__name__)
-cw = chatwork.setup(114514,"aa")
 API_TOKEN = os.getenv("chatwork_api_token")
-
+cw = chatwork.setup(114514,API_TOKEN)
 SECRET_TOKEN = None
 
-bot_account_id = 11156582 
+bot_account_id = 11156582
+@client.event
+async def get_messages(message):
+    if message.author.bot:
+        return
+    if message.content:
+       cw.messagesend(f"[info][title]{message.author.display_name}さんのコメント[/title]{message.content}[/info]")
+
 @app.route("/",methods=["POST"])
 def root():
     return "ok"
@@ -30,7 +41,10 @@ def webhook():
 
     cw = chatwork.setup(room_id,API_TOKEN)
     
+
     return jsonify({"status": "ok"}),200
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+    discord_token = str(os.getenv("discord_token"))
+    client.run(discord_token)
